c35459cd7f152de4e439c7e27f7f0897a2ee14cf
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-17 15:09:22 -0500

    MERFISH_ICON.R

--- MERFISH_ICON.R
+++ MERFISH_ICON.R
@@ -1,18 +1,16 @@
-# MERFISH Data analysis 
 
-library(dplyr)
-library(ggplot2)
-library(patchwork)
-library(Seurat)
 
-# Create Seurat object
 
+
+
+vizgen.input <- ReadVizgen(data.dir = "/path to data/", type = "centroids")
 ## remove the Blank-* control probes
 blank_index<- which(stringr::str_detect(rownames(vizgen.input$transcripts), "^Blank"))
-# blanks are empty
-transcripts<-vizgen.input$transcripts
+
+transcripts<-vizgen.input$transcripts[-blank_index, ]
 
 dim(vizgen.input$transcripts)
+
 
 vizgen.obj<- CreateSeuratObject(counts = transcripts, assay = "Vizgen")
 
@@ -31,4 +29,14 @@ coords <- CreateFOV(
 
 vizgen.obj[["vizgen.obj"]] <- coords
 
+# Remove cells with fewer than 10 transcripts
+vizgen.obj<- subset(x=vizgen.obj,subset = nCount_Vizgen > 10)
 
+vizgen.obj <- NormalizeData(vizgen.obj, normalization.method = "LogNormalize", scale.factor = 10000) %>%
+  ScaleData() 
+vizgen.obj <- RunPCA(vizgen.obj, npcs = 30, features = rownames(vizgen.obj))
+vizgen.obj <- RunUMAP(vizgen.obj, dims = 1:30)
+vizgen.obj <- FindNeighbors(vizgen.obj, reduction = "pca", dims = 1:30)
+vizgen.obj <- FindClusters(vizgen.obj, resolution = 0.8)
+
+saveRDS(vizgen.obj, file ="ICON_MERFISH.rds")
