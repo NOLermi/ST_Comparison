1ccffb5a2d0ad7039946474c0e4651af8a35270b
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-17 13:20:35 -0500

    CosMx_ICON.R

--- CosMx_ICON.R
+++ CosMx_ICON.R
@@ -30,3 +30,34 @@ Icon2<- subset(x=Icon1,subset = nCount_Nanostring > 30)
 
 Geomean <- exp(mean(log(Icon2$Area)))
 Icon2<-  subset(Icon2, subset = Area < 5*Geomean)
+
+# Merge datasets
+library(harmony)
+
+ICON<- merge(Icon1, y = Icon2, add.cell.ids = c("Icon1", "Icon2"), project = "ICON")
+ICON
+head(slot(object = ICON, name = "meta.data"))
+
+unique(ICON$slide_ID)
+# create a list of the original data that we loaded to start with
+ICON <- Seurat::NormalizeData(ICON,normalization.method = "LogNormalize",verbose = T)
+ICON <- FindVariableFeatures(ICON,selection.method = "vst", nfeatures = 1000) 
+ICON<- ScaleData(ICON,verbose = T)
+ICON <- RunPCA(ICON,npcs = 30, verbose = T)
+
+
+## Integrate with HARMONY
+ICON <- ICON %>% 
+  RunHarmony("slide_ID", plot_convergence = TRUE)
+
+ICON <- ICON %>% 
+  RunUMAP(reduction = "harmony", dims = 1:30) %>% 
+  FindNeighbors(reduction = "harmony", dims = 1:30) %>% 
+  FindClusters(resolution = 0.8) %>% 
+  identity()
+
+# Find cell markers per clusters
+ICON <- JoinLayers(ICON)
+ICON_markers <- FindAllMarkers(ICON, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.5)
+
+saveRDS(ICON,file="ICON_CosMx.rds")
