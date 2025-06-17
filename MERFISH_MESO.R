ce8cfaeb774164d8dbe917acd0e903ed39edc30f
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-17 14:58:06 -0500

    MERFISH_MESO.R

--- MERFISH_MESO.R
+++ MERFISH_MESO.R
@@ -1,4 +1,11 @@
+# MERFISH Data analysis 
 
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+library(Seurat)
+
+# Create Seurat object
 
 vizgen.input <- ReadVizgen(data.dir = "/path to data/", type = "centroids")
 ## remove the Blank-* control probes
@@ -8,7 +15,7 @@ transcripts<-vizgen.input$transcripts[-blank_index, ]
 
 dim(vizgen.input$transcripts)
 
-# Create seurat object
+
 vizgen.obj<- CreateSeuratObject(counts = transcripts, assay = "Vizgen")
 
 cents <- CreateCentroids(vizgen.input$centroids)
@@ -26,5 +33,14 @@ coords <- CreateFOV(
 
 vizgen.obj[["vizgen.obj"]] <- coords
 
-
+# Remove cells with fewer than 10 transcripts
 vizgen.obj<- subset(x=vizgen.obj,subset = nCount_Vizgen > 10)
+
+vizgen.obj <- NormalizeData(vizgen.obj, normalization.method = "LogNormalize", scale.factor = 10000) %>%
+  ScaleData() 
+vizgen.obj <- RunPCA(vizgen.obj, npcs = 30, features = rownames(vizgen.obj))
+vizgen.obj <- RunUMAP(vizgen.obj, dims = 1:30)
+vizgen.obj <- FindNeighbors(vizgen.obj, reduction = "pca", dims = 1:30)
+vizgen.obj <- FindClusters(vizgen.obj, resolution = 0.8)
+
+saveRDS(vizgen.obj, file ="MESO_MERFISH.rds")
